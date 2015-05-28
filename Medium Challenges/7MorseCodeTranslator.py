02e3c9fc6db816f84ebda33fef5da8228b3be4d4
omrihq <omrihq@gmail.com>
2015-05-27 22:47:01 -0700

    Morse code translator much more pythonic

--- Medium Challenges/7MorseCodeTranslator.py
+++ Medium Challenges/7MorseCodeTranslator.py
@@ -1,39 +1,33 @@
-#alphabet
-alphabet = {"A" : ".-",        
-"B" : "-...",      
-"C" : "-.-.",     
-"D" : "-..",       
-"E" : ".",         
-"F" : "..-.",      
-"G" : "--.",       
-"H" : "....",      
-"I" : "..",        
-"J" : ".---",      
-"K" : "-.-",      
-"L" : ".-..",      
-"M" : "--",        
-"N" : "-.",
-"O" : "---",
-"P" : ".--.",
-"Q" : "--.-",
-"R" : ".-.",
-"S" : "...",
-"T" : "-",
-"U" : "..-",
-"V" : "...-",
-"W" : ".--",
-"X" : "-..-",
-"Y" : "-.--",
-"Z" : "--.."}
-
+alphabet = {".-"   : "A",          
+"-..." : "B",          
+"-.-." : "C",          
+"-.."  : "D",          
+"."    : "E",          
+"..-." : "F",          
+"--."  : "G",          
+"...." : "H",          
+".."   : "I",          
+".---" : "J",          
+"-.-"  : "K",          
+".-.." : "L",          
+"--"   : "M",          
+"-." : "N",
+"---" : "O",
+".--." : "P",
+"--.-" : "Q",
+".-." : "R",
+"..." : "S",
+"-" : "T",
+"..-" : "U",
+"...-" : "V",
+".--" : "W",
+"-..-" : "X",
+"-.--" : "Y",
+"--.." : "Z",
+"/" : " "}
 ##try using this: .... . .-.. .-.. --- / -.. .- .. .-.. -.-- / .--. .-. --- --. .-. .- -- -- . .-. / --. --- --- -.. / .-.. ..- -.-. -.- / --- -. / - .... . / -.-. .... .- .-.. .-.. . -. --. . ... / - --- -.. .- -.--
 
 code = raw_input("Enter the morse code you\'d like to translate, with each word divided by a \" / \" and each letter by a space")
-morse = code.split(" / ")
-#Not very pythonic...
-for word in morse:
-        word = word.split()
-        for morse_letter in word:
-                for letter, key in alphabet.iteritems():
-                        if morse_letter == key:
-                                print letter,
+#Beautifully pythonic if I do say so myself
+print (''.join(alphabet[letter] for letter in code.split()))
+
